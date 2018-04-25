dceccc1174bc66ed112fb40fa7c51aed5ef063b9
Christopher Hicks <chicks.net@gmail.com>
2018-04-25 09:15:36 -0700

    load messages into SQL

--- create.sql
+++ create.sql
@@ -1,4 +1,1 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE fb_items (id INTEGER PRIMARY KEY ASC, type TEXT, participant TEXT, timestamp TEXT, content TEXT, redact INT);
-COMMIT;
+CREATE TABLE fb_items (id INTEGER PRIMARY KEY ASC, type TEXT, participant TEXT, timestamp TEXT, speaker TEXT, content TEXT, content_html TEXT, redact INT)
